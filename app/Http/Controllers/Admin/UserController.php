0be740d18b2cc008c6d971f13b9121a056c8e073
Najmul Hossain <phpnajmul@gmail.com>
2024-04-01 15:08:26 +0600

    I am adding server side validation in user create & update

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -19,7 +19,9 @@ class UserController extends Controller
     public function store()
     {
         request()->validate([
-            'email' => 'required|unique:users,email'
+            "name"=> "required|max:255",
+            'email' => 'required|unique:users,email',
+            'password'=> 'required|min:8',
         ]);
 
         return User::create([
@@ -33,7 +35,9 @@ class UserController extends Controller
     public function update(User $user)
     {
         request()->validate([
-            'email' => 'required|unique:users,email,'.$user->id
+            'name'=> 'required|max:255',
+            'email' => 'required|unique:users,email,'.$user->id,
+            'password'=> 'sometimes|min:8',
         ]);
 
         $user->update([
@@ -44,7 +48,8 @@ class UserController extends Controller
 
         return $user;
     }
-
+    
+    
 
 
 }
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::get('csrf', function () {
+    return csrf_token();
+});
+
+
 Route::get('/', function () {
     return view('welcome');
 });
